57d61ec2b211b3199737f684b6e62526fb833c09
Tanzeel550 <tanzeelahmed550@gmail.com>
2021-01-28 18:46:10 +0500

    changes made to the project

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ mongoose
     });
 
 // Starting the server
-const port = process.env.SERVER_PORT || 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
